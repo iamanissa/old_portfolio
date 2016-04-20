92c42d7103e0a2dcace219a750cdbea150958a45
Anissa Jones <anissajones10@gmail.com>
2016-04-20 09:08:00 -0400

    Tries out angular.

--- app.js
+++ app.js
@@ -2,5 +2,26 @@
 
 (function(){
   angular
-  .module("portfolio", []);
+  .module("portfolio", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider", RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("resume", {
+      url:"/resume",
+      templateUrl: "./resume/resume.html"
+    })
+    .state("projects", {
+      url: "/projects",
+      templateUrl: "./projects/projects.html"
+    })
+    .state("aboutme", {
+      url: "/aboutme",
+      templateUrl: "./aboutme/aboutme.html"
+    });
+  }
 })();
