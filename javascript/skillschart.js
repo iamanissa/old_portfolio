cabf5faf702ea8fe4447c1a9953a262e535e4d0f
Anissa Jones <anissajones10@gmail.com>
2016-05-04 15:29:04 -0400

    Fixes y-axis names. Fixes values for x-axis. Begins to work with understanding chartjs colors.

--- javascript/skillschart.js
+++ javascript/skillschart.js
@@ -3,39 +3,39 @@
 function createSkillsChart(){
   var chrt = document.getElementById('mycanvas').getContext('2d');
   var data = {
-    labels: ["HTML/CSS", "JavaScript", "AngularJS", "Ruby", "Ruby on Rails" ], //x-axis
+    labels: ["HTML", "CSS/SASS", "Bootstrap", "JavaScript", "MEAN Stack", "Ruby", "Ruby on Rails", "PostgreSQL", "Git/github", "Wordpress/PHP" ], //x-axis
     datasets: [
       {
         label: "Skills",
-        fillColor: "rgba(220, 220, 220, 0.8)",
-        strokeColor: "rgba(220, 220, 220, 0.8)",
-        highlightFill: "rgba(220, 220, 220, 0.75)",
+        fillColor: "rgba(255, 0, 0, 1)",
+        strokeColor: "rgba(0,100,0,1)",
+        highlightFill: "rgba(30,144,255,1)",
         highlightStroke: "rgba(220, 220, 220,1)",
-        data: [65, 59, 80, 81, 56, 55, 40] //y-axis
+        data: [78, 60, 65, 80, 50, 82, 86, 65, 50, 25] //y-axis
+
       }
-    ]
+    ],
   };
 
   var options = {
     scaleLabel : function (valuePayload) {
         if(Number(valuePayload.value)===0)
         return "zilch";
-        if(Number(valuePayload.value)===20)
-        return 'request added';
-        if(Number(valuePayload.value)===40)
-        return 'request viewed';
-        if(Number(valuePayload.value)===60)
-        return 'request accepted';
-        if(Number(valuePayload.value)===80)
-        return 'request solved';
+        if(Number(valuePayload.value)===25)
+        return 'Basic, and interested';
+        if(Number(valuePayload.value)===50)
+        return 'Medium, and improving';
+        if(Number(valuePayload.value)===75)
+        return "High, I'm pretty awesome :-) ";
         if(Number(valuePayload.value)===100)
-        return 'solving confirmed';
+        return "Wizard Ninja Jedi Web Dev Master";
         },
 
     scaleOverride: true,
-    scaleSteps: 5,
-    scaleStepWidth: 20,
-    scaleStartValue: 0
+    scaleSteps: 4,
+    scaleStepWidth: 25,
+    scaleStartValue: 0,
+    scaleShowVerticalLines: false
    };
 
    var mySkillsChart = new Chart(chrt).Bar(data, options);
