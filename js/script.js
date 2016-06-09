369f0be0fc3cbdc27a1b7e9f16c935062d6099d2
Anissa Jones <anissajones10@gmail.com>
2016-06-09 16:36:26 -0400

    Now the elements move based off scroll amount.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 $(window).scroll(function(){
   // when the window scrolls, tell me where zero is in relation to the top
   var wScroll = $(this).scrollTop();
-  console.log(wScroll);
+
+  $(".anissalogo").css({
+    'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+  });
+
+  $(".yellow-flower").css({
+    'transform' : 'translate(0px, -'+ wScroll /20 +'%)'
+  });
 
 });
