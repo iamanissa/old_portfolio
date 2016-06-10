28682832f497e6a302b33b7f9e0fc61f5929438f
Anissa Jones <anissajones10@gmail.com>
2016-06-10 16:17:40 -0400

    adds function to add class is showing and fade-in animation.

--- js/script.js
+++ js/script.js
@@ -14,4 +14,18 @@ $(window).scroll(function(){
     'transform' : 'translate('+ wScroll /80 +'%, '+ wScroll /2 +'%)'
   });
 
+  // ==========  About me pics moving =============
+
+  // if the scroll number is greater than whatever the top of the window is minus the actual windows height / 1.5 (for midpoint of window)
+  if(wScroll > $('.gridAbout').offset().top - ($(window).height() / 1.5)){
+
+    //iterate through each item with the class gridAbout & icon-about
+    $('.gridAbout .icon-about').each(function(i){
+
+      //set a timeout so they don't fad in at the same time and add the class is-showing to each one.
+      setTimeout(function(){
+        $('.gridAbout .icon-about').eq(i).addClass('is-showing');
+      }, 150 * (i+1));
+    });
+  }
 });
